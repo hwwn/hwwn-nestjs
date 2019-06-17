464b8149e19bfb160414d6ff960a8cac9499f6ae
hwwn <hwwn@live.com>
2019-06-17 08:33:47 +0800

    feature: 请求 request

---
+++ .history/src/posts/posts.controller_20190617003508.ts
@@ -0,0 +1,13 @@
+import { Controller, Get } from '@nestjs/common';
+
+@Controller('posts')
+export class PostsController {
+  @Get()
+  index() {
+    return [
+      {
+        title: 'hello~',
+      },
+    ];
+  }
+}
