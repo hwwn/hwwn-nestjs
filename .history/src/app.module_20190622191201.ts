63791869007242b9aef347dc2c80b713d3fe8eff
hwwn <hwwn@live.com>
2019-06-22 22:16:47 +0800

    任务：定义服务方法

--- .history/src/app.module_20190622191201.ts
+++ .history/src/app.module_20190622191201.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostsController } from './posts/posts.controller';
+import { DemoService } from './demo/demo.service';
 import { DemoService } from './posts/providers/demo/demo.service';
 
 @Module({
